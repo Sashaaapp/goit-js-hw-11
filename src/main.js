c770764725027b065cd59a801d884d51a68fbc04
Sashaaapp <sashaaapp1980@gmail.com>
2024-01-22 20:50:02 +0200

    main.js and index.html update

--- src/main.js
+++ src/main.js
@@ -8,22 +8,21 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
 document.addEventListener('DOMContentLoaded', function () {
-  const loaderContainer = document.getElementById('loaderContainer');
+  const loaderContainer = document.getElementById('loader-container');
   hideLoader(loaderContainer);
 
   const searchForm = document.getElementById('searchForm');
   const searchInput = document.getElementById('searchInput');
-  const gallery = document.getElementById('gallery');
   const startBtn = document.querySelector('.btn');
+  const gallery = document.getElementById('gallery');
+
   const apiKey = '41934305-8f787e974a2ef1238ff7fef77';
 
   searchForm.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    // Відображення індикатора завантаження
-    showLoader();
+    showLoader(loaderContainer);
 
     const searchTerm = searchInput.value.trim();
     if (searchTerm === '') {
@@ -31,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function () {
         title: 'Error',
         message: 'Please enter a search term',
       });
-      hideLoader();
+      hideLoader(loaderContainer);
       return;
     }
 
@@ -40,15 +39,22 @@ document.addEventListener('DOMContentLoaded', function () {
       key: apiKey,
       q: searchTerm,
       image_type: 'photo',
-      orientation: 'horizontal',
       safesearch: true,
+      orientation: 'horizontal',
       per_page: 9,
     };
 
     fetch(`${apiUrl}?${new URLSearchParams(requestData)}`)
       .then(response => response.json())
       .then(data => {
-        displayImages(data.hits);
+        if (data.hits.length === 0) {
+          iziToast.error({
+            title: 'Error',
+            message: 'No images found for the provided search term',
+          });
+        } else {
+          displayImages(data.hits);
+        }
       })
       .catch(() => {
         iziToast.error({
@@ -61,13 +67,13 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   });
 
-  function showLoader() {
+  function showLoader(loaderContainer) {
     if (loaderContainer) {
       loaderContainer.style.display = 'block';
     }
   }
 
-  function hideLoader() {
+  function hideLoader(loaderContainer) {
     if (loaderContainer) {
       loaderContainer.style.display = 'none';
     }
@@ -90,7 +96,6 @@ document.addEventListener('DOMContentLoaded', function () {
       const card = document.createElement('div');
       card.className = 'gallery-item';
 
-      // Обгортаємо картку зображення в посилання для SimpleLightbox
       card.innerHTML = `
       <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="Likes: ${image.likes}, Views: ${image.views}, Comments: ${image.comments}, Downloads: ${image.downloads}">
           <img src="${image.webformatURL}" alt="${image.tags}" data-src="${image.largeImageURL}" data-caption="Likes: ${image.likes}, Views: ${image.views}, Comments: ${image.comments}, Downloads: ${image.downloads}">
@@ -124,5 +129,124 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     lightbox.refresh();
+    // Оновлення галереї
   }
 });
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const loaderContainer = document.getElementById('loader-сontainer');
+//   hideLoader(loaderContainer);
+
+//   const searchForm = document.getElementById('searchForm');
+//   const searchInput = document.getElementById('searchInput');
+//   const gallery = document.getElementById('gallery');
+//   const startBtn = document.querySelector('.btn');
+//   const apiKey = '41934305-8f787e974a2ef1238ff7fef77';
+
+//   searchForm.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     showLoader(loaderContainer);
+
+//     const searchTerm = searchInput.value.trim();
+//     if (searchTerm === '') {
+//       iziToast.error({
+//         title: 'Error',
+//         message: 'Please enter a search term',
+//       });
+//       hideLoader(loaderContainer);
+//       return;
+//     }
+
+//     const apiUrl = 'https://pixabay.com/api/';
+//     const requestData = {
+//       key: apiKey,
+//       q: searchTerm,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: true,
+//       per_page: 9,
+//     };
+
+//     fetch(`${apiUrl}?${new URLSearchParams(requestData)}`)
+//       .then(response => response.json())
+//       .then(data => {
+//         displayImages(data.hits);
+//       })
+//       .catch(() => {
+//         iziToast.error({
+//           title: 'Error',
+//           message: 'Failed to fetch images. Please try again later.',
+//         });
+//       })
+//       .finally(() => {
+//         hideLoader(loaderContainer);
+//       });
+//   });
+
+//   function showLoader(loaderContainer) {
+//     if (loaderContainer) {
+//       loaderContainer.style.display = 'block';
+//     }
+//   }
+
+//   function hideLoader(loaderContainer) {
+//     if (loaderContainer) {
+//       loaderContainer.style.display = 'none';
+//     }
+//   }
+
+//   function displayImages(images) {
+//     gallery.innerHTML = '';
+
+//     if (images.length === 0) {
+//       iziToast.info({
+//         title: 'Info',
+//         message:
+//           'Sorry, there are no images matching your search query. Please try again!',
+//       });
+//       hideLoader();
+//       return;
+//     }
+
+//     images.forEach(image => {
+//       const card = document.createElement('div');
+//       card.className = 'gallery-item';
+
+//       // Обгортаємо картку зображення в посилання для SimpleLightbox
+//       card.innerHTML = `
+//       <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="Likes: ${image.likes}, Views: ${image.views}, Comments: ${image.comments}, Downloads: ${image.downloads}">
+//           <img src="${image.webformatURL}" alt="${image.tags}" data-src="${image.largeImageURL}" data-caption="Likes: ${image.likes}, Views: ${image.views}, Comments: ${image.comments}, Downloads: ${image.downloads}">
+//         </a>
+//         <div class="image-stats">
+//       <div class="stat-item">
+//         <p class="stat-label">Likes:</p>
+//         <p class="stat-value">${image.likes}</p>
+//       </div>
+//       <div class="stat-item">
+//         <p class="stat-label">Views:</p>
+//         <p class="stat-value">${image.views}</p>
+//       </div>
+//       <div class="stat-item">
+//         <p class="stat-label">Comments:</p>
+//         <p class="stat-value">${image.comments}</p>
+//       </div>
+//       <div class="stat-item">
+//         <p class="stat-label">Downloads:</p>
+//         <p class="stat-value">${image.downloads}</p>
+//       </div>
+//     </div>
+//       `;
+
+//       gallery.appendChild(card);
+//     });
+
+//     const lightbox = new SimpleLightbox('.gallery a', {
+//       captionsData: 'alt',
+//       captionDelay: 250,
+//     });
+
+//     lightbox.refresh();
+//   }
+// });
